c22549e32670b4acd34159cae129534f466de6c6
Ste9h3n <69769298+Ste9h3n@users.noreply.github.com>
2021-02-25 17:37:26 -0500

    added a few things

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,7 @@
 const discord = require("discord.js");
 const dotenv = require("dotenv");
 
+const express = require("express");
 const client = new discord.Client();
 
 dotenv.config({ path: __dirname.replace("src", "private/") + ".env" });
@@ -9,3 +10,14 @@ client.login(process.env.BOT_TOKEN);
 client.on("ready", () => {
     console.log("Sparky's ready.");
 })
+
+client.on("message", (msg) => {
+    if (msg.author.bot) return;
+
+    if (msg.content.startsWith(`<@!${client.user.id}>`)) {
+        msg.channel.send("https://github.com/Ste9h3n/Sparky");
+    }
+
+    if (!msg.content.startsWith(process.env.BOT_PREFIX)) return;
+})
+
